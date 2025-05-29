611ebe1c8a6906beea13a69dee3441b30bf08e6c
furkanayyildiz55 <furkanayyildiz55@hotmail.com>
2025-05-29 20:22:19 +0300

    29.05.2025 -2

--- AspNetCoreAuthServer.API/Program.cs
+++ AspNetCoreAuthServer.API/Program.cs
@@ -1,6 +1,17 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using SharedLibrary.Configurations;
+using SharedLibrary.Services;
 using UdemyAuthServer.Core.Configuration;
+using UdemyAuthServer.Core.Models;
+using UdemyAuthServer.Core.Repositories;
+using UdemyAuthServer.Core.Services;
+using UdemyAuthServer.Core.UnitOfWork;
+using UdemyAuthServer.Data;
+using UdemyAuthServer.Data.Repositories;
+using UdemyAuthServer.Service.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,8 +22,58 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//DI
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped(typeof(IServiceGeneric<,>), typeof(ServiceGeneric<,>));
+builder.Services.AddScoped<IUnitOfWork,UnitOfWork>();
+
+//DB
+builder.Services.AddDbContext<AppDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"), sqlOptions =>
+    {
+        //migration iþlemleri data katmanýnda olacaðý için
+        sqlOptions.MigrationsAssembly("UdemyAuthServer.Data");
+    });
+});
+
+//IDENTITY
+builder.Services.AddIdentity<UserApp, IdentityRole>(Opt =>
+{
+    Opt.User.RequireUniqueEmail = true;
+    Opt.Password.RequireNonAlphanumeric = false;
+}).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+
+//CONFÝGURATION
 builder.Services.Configure<CustomTokenOption>(builder.Configuration.GetSection("TokenOption"));
 builder.Services.Configure<List<Client>>(builder.Configuration.GetSection("Clients"));
+
+//CONFÝGURATION INSTANCE
+var tokenOption = builder.Configuration.GetSection("TokenOption").Get<CustomTokenOption>();
+
+// TOKEN AUTH
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, opts =>
+{
+    opts.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+    {
+        ValidIssuer = tokenOption.Issuer,
+        ValidAudience = tokenOption.Audience[0],
+        IssuerSigningKey = SignService.GetSymmetricSecurityKey(tokenOption.SecurityKey),
+
+        ValidateIssuerSigningKey = true,
+        ValidateAudience = true,
+        ValidateIssuer = true,
+        ValidateLifetime = true,
+        ClockSkew = TimeSpan.Zero
+    };
+});
 
 var app = builder.Build();
 
@@ -26,6 +87,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+
 
 app.MapControllers();
 
