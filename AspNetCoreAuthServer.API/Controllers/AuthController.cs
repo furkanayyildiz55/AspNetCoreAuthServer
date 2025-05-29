a4ffd1a68937933bd753b6fad5968bea8c709a7d
furkanayyildiz55 <furkanayyildiz55@hotmail.com>
2025-05-29 20:51:04 +0300

    AuthController yazıldı

---
+++ AspNetCoreAuthServer.API/Controllers/AuthController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UdemyAuthServer.Core.DTOs;
+using UdemyAuthServer.Core.Services;
+
+namespace AspNetCoreAuthServer.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class AuthController : CustomBaseController
+    {
+        private readonly IAuthenticationService _authenticationService;
+
+        public AuthController(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateToken(LoginDto loginDto)
+        {
+            var result = await _authenticationService.CreateTokenAsync(loginDto);
+            return ActionResultInstance(result);
+        }
+
+        [HttpPost]
+        public IActionResult CreateTokenByClient(ClientLoginDto clientLoginDto)
+        {
+            var result = _authenticationService.CreateTokenByClient(clientLoginDto);
+
+            return ActionResultInstance(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RevokeRefreshToken(RefreshTokenDto refreshTokenDto)
+        {
+            var result = await _authenticationService.RevokeRefreshToken(refreshTokenDto.Token);
+
+            return ActionResultInstance(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTokenByRefreshToken(RefreshTokenDto refreshTokenDto)
+        {
+            var result = await _authenticationService.CreateTokenByRefreshToken(refreshTokenDto.Token);
+
+            return ActionResultInstance(result);
+        }
+    }
+}
